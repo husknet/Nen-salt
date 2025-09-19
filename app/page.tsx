fc3354730f1c159270726db29cb7f62e32cdece7
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-09-19 12:25:01 -0400

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,5 @@
-export const dynamic = "force-dynamic";
-
 "use client";
-
+export const dynamic = "force-dynamic";
 import EmailProcessor from "./EmailProcessor";
 
 export default function Page() {

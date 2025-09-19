69deafbd9bf190d5aff9dd7281d5dda0ce9361f5
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-09-19 12:37:36 -0400

    Delete app directory

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-"use client";
-export const dynamic = "error";
-export const fetchCache = "force-no-store";
-import EmailProcessor from "./EmailProcessor";
-
-export default function Page() {
-  return <EmailProcessor />;
-}

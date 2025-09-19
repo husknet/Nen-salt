5af59f29c67d84e1fb9301ddcfbf1d824d71ea23
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-09-19 11:26:12 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css";
+import ".styles/globals.css";
 import type { Metadata } from "next";
 
 export const metadata: Metadata = {
@@ -13,3 +13,4 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
     </html>
   );
 }
+

992298879d1fff339047a86281ff9061a7d7c2f6
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-09-19 19:20:20 +0300

    Add files via upload

--- app/page.tsx
+++ app/page.tsx
@@ -2,13 +2,8 @@ export const dynamic = "force-dynamic";
 
 "use client";
 
-import { Suspense } from "react";
 import EmailProcessor from "./EmailProcessor";
 
 export default function Page() {
-  return (
-    <Suspense fallback={<div className=\"text-white\">Loading...</div>}>
-      <EmailProcessor />
-    </Suspense>
-  );
+  return <EmailProcessor />;
 }

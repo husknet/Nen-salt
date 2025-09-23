d3e7d8c03e9ad7bc2cd8956ae22593bfb89c8ef3
hurt the kid <127146284+husknet@users.noreply.github.com>
2025-09-23 10:41:15 -0400

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,5 @@
 import type { AppProps } from "next/app";
-import "../styles/globals.css"; // ✅ Import Tailwind CSS globally
+import "../styles/globals.css"; // ✅ Import Tailwind CSS globallyj
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
